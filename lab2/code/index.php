8d689233c39d05376bbed636431a669d099099cd
Vadimです <v140204050607@mail.ru>
2024-03-01 17:59:54 +0200

    complete Task11

--- lab2/code/index.php
+++ lab2/code/index.php
@@ -1,28 +1,8 @@
 <?php
 
-$lor1 = 25;
-$lor2 = 15;
+$num_languages = 4;
+$months = 11;
+$days = $months * 16;
+$days_per_language = $days / $num_languages;
 
-echo $lor1;
-echo "<br>"; //для браузера
-echo "\n"; //для текстового формата
-echo $lor2;
-
-echo "<br>";
-echo "\n";
-
-$lor3 = 3.14;
-echo $lor3;
-
-echo "<br>";
-echo "\n";
-
-echo $lor1 - $lor2 + 2;
-
-echo "<br>";
-echo "\n";
-
-$last_month = 1187.23;
-$this_month = 1089.98;
-
-echo $last_month - $this_month;
+echo $days_per_language;
