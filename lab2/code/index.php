23c94c8638a187f9c33d08a18465f80aa8a7c283
Vadimです <v140204050607@mail.ru>
2024-03-01 17:53:17 +0200

    complete Task2

--- lab2/code/index.php
+++ lab2/code/index.php
@@ -1,9 +1,28 @@
 <?php
 
-$very_bad_unclear_name = "15 chicken wings";
+$lor1 = 25;
+$lor2 = 15;
 
-$order = & $very_bad_unclear_name;
-$order .= " with cheese sauce";
+echo $lor1;
+echo "<br>"; //для браузера
+echo "\n"; //для текстового формата
+echo $lor2;
 
-echo "\nYour order is:$very_bad_unclear_name.";
+echo "<br>";
+echo "\n";
 
+$lor3 = 3.14;
+echo $lor3;
+
+echo "<br>";
+echo "\n";
+
+echo $lor1 - $lor2 + 2;
+
+echo "<br>";
+echo "\n";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+
+echo $last_month - $this_month;
